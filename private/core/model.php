a76b35430fecac47da7277ee3f471843ecca04b9
Saeid alebraheem <560739@edu.rocmn.nl>
2023-03-29 10:53:10 +0200

    CRUD

--- private/core/model.php
+++ private/core/model.php
@@ -25,5 +25,34 @@ class Model extends Database
         return $this->query($query);
     }
 
+    public function addObject($data)
+    {
+        
+        $keys = array_keys($data);
+        $columns = implode(",", $keys);
+        $values = implode(",:", $keys);
+        $query = "INSERT INTO $this->table ( $columns ) VALUES (:$values);";       
+        return $this->query($query,$data);   
+    }
+    public function updateObject($id, $data)
+    {
+        $str = "";
+        foreach($data as $key => $value)
+        {
+            $str .= $key . "=:" . $key. ",";
+        }
+        $str = trim($str,",");
+        $data['id'] = $id;
+        $query = "UPDATE $this->table SET $str WHERE id = :id"; 
+        echo $query;    
+        return $this->query($query,$data);   
+    }
+
+    public function deleteObject($id)
+    {
+        $query = "DELETE FROM $this->table WHERE id = :id"; 
+        $data['id'] = $id;
+        return $this->query($query,$data);  
+    }
     
 }
