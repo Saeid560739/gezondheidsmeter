bbee639c5f5461075cb48e93369acc4fcb15ae28
Saeid alebraheem <560739@edu.rocmn.nl>
2023-03-30 11:54:48 +0200

    login style

--- private/views/login.view.php
+++ private/views/login.view.php
@@ -1,25 +1,39 @@
-<?php $this->view('inclodes/header')?>
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
-    <h1 class="active"> Inloggen </h1>
 
-    <!-- Login Form -->
-    <form method="post">
-      <input type="text" id="email" class="fadeIn second" name="email" placeholder="email">
-      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Inloggen">
-      <?php foreach($errors as $error):?>
-      <div id="errors" style="color:red"><?=$error?><div>
-      <?php endforeach;?>
-    </form>
+<!DOCTYPE html>
+<html lang="en" id="user_body">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="John Doe">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="http://localhost/gezondheidsmeter/public/style/user_style.css">
+    </head>
+    <body>
 
-    <!-- Remind Passowrd -->
-    <div id="formFooter">
-      <a class="underlineHover" href="http://localhost/gezondheidsmeter/public/accountAanmaken">Account aanmaken</a>
-    </div>
+      <div class="wrapper fadeInDown">
+        <div id="formContent">
+          <!-- Tabs Titles -->
+          <h1 class="active"> Inloggen </h1>
 
-  </div>
-</div>
+          <!-- Login Form -->
+          <form method="post">
+            <input type="text" id="email" class="fadeIn second" name="email" placeholder="email">
+            <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
+            <input type="submit" class="fadeIn fourth" value="Inloggen">
+            <?php foreach($errors as $error):?>
+            <div id="errors" style="color:red"><?=$error?><div>
+            <?php endforeach;?>
+          </form>
+
+          <!-- Remind Passowrd -->
+          <div id="formFooter">
+            <a class="underlineHover" href="http://localhost/gezondheidsmeter/public/accountAanmaken">Account aanmaken</a>
+          </div>
+
+        </div>
+      </div>
         
-<?php $this->view('inclodes/footer')?>
+    </body>
+</html>
